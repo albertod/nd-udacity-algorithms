5b6f8df6b49e9e542238579cd6bfb82f24debded
Alberto Di Martino <alberto.dimartino@samsung.com>
2019-04-29 14:28:03 -0700

    Solve P2

--- module1/P0/Task2.py
+++ module1/P0/Task2.py
@@ -1,16 +1,4 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files
-"""
-import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-
+from Helper import *
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
@@ -19,4 +7,23 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+import csv
 
+
+def getCalls():
+    calls = []
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            calls.append(Call(row[0], row[1], row[2], row[3]))
+
+    return calls
+
+
+def solveP2():
+    calls = getCalls()
+    longest_call = max(calls, key=lambda call: int(call.time))
+    print(f'{longest_call.src_number} spent the longest time, {longest_call.time} seconds, on the phone during September 2016.')
+
+
+solveP2()
