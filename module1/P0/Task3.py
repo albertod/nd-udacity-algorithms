33c2c5950f0bb17e0719a7dee1026c9e799feec3
Alberto Di Martino <albertodmartino@gmail.com>
2019-05-01 20:13:00 -0700

    Finishn project1

--- module1/P0/Task3.py
+++ module1/P0/Task3.py
@@ -1,16 +1,4 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files.
-"""
-import csv
-
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+from Helper import *
 
 """
 TASK 3:
@@ -28,6 +16,17 @@ in Bangalore.
  - Telemarketers' numbers have no parentheses or space, but they start
    with the area code 140.
 
+# Input Calls records
+# Output:
+1. Get areaa code and mobile prefixes called from (080)
+2. Get the % of calls made from (080) to (080)
+
+# procedure
+1.get a lsit with all the records where the src_number starts with (080)
+2. do a loop where we do 2 things for each record
+ a. save the area code || prefix to a set (that will be sorted) prefix_codes
+ b. If the dst_number's prefix is (080) save the number to a list --> banagalore_local_calls
+
 Print the answer as part of a message:
 "The numbers called by people in Bangalore have codes:"
  <list of codes>
@@ -43,3 +42,50 @@ Print the answer as a part of a message::
 to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
+
+import csv
+
+def getBangaloreCalls():
+    prefixes = set()
+    calls_to_bangalore = list()
+    calls_from_bangalore = 0
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            call = Call(row[0], row[1], row[2], row[3])
+            if (row[0].startswith('(080)')): #It is a call from bangalore
+              calls_from_bangalore +=1
+               # check is not telemarketer
+              if not call.dst_number.startswith('140'):
+                if call.dst_number.startswith("("):
+                  # Mobile number
+                  prefixes.add(call.dst_number[call.dst_number.find("(") + 1:call.dst_number.find(")")])
+                else:
+                  # Landline
+                  prefixes.add(call.dst_number.split(" ")[0])
+                
+                if call.dst_number.startswith('(080)'):
+                  calls_to_bangalore.append(call.dst_number)
+
+      
+    percentage_calls_to_bangalore = (len(calls_to_bangalore)/calls_from_bangalore) * 100
+    
+    return prefixes, percentage_calls_to_bangalore
+
+
+def printPhoneCodes(codeList):
+  print("The numbers called by people in Bangalore have codes:")
+  sorted(codeList)
+  for code in codeList:
+    print(code)
+
+def solveP3():
+  prefixes, calls_percentage = getBangaloreCalls()
+  
+  # Print response
+  printPhoneCodes(prefixes)
+  print()
+  print(f"{round(calls_percentage, 2)} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")
+
+solveP3()
+
--- module1/P0/Task4.py
+++ module1/P0/Task4.py
@@ -1,17 +1,4 @@
-"""
-Read file into texts and calls.
-It's ok if you don't understand how to read files.
-"""
-import csv
-
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
-
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
-
+from Helper import *
 """
 TASK 4:
 The telephone company want to identify numbers that might be doing
@@ -25,3 +12,50 @@ Print a message:
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
 
+def getCallAndText():
+    """
+    Returns 3 dictionaries in a tuple
+    1. All the numbers receiving calls
+    2. All the numbers sending text messages
+    3. All the numbers receiving text messages
+    """
+    receving_calls = {}
+    sending_texts = {}
+    receiving_texts = {}
+    src_calls = set()
+    
+    with open('texts.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            sending_texts[row[0]] = True
+            receiving_texts[row[1]] = True
+
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            receving_calls[row[1]] = True
+            src_calls.add(row[0])
+
+    return receving_calls, sending_texts, receiving_texts, src_calls
+
+def getPossibleTelemarketers(receving_calls, sending_texts, receiving_texts, src_calls):
+    results = set()
+    for call in src_calls:
+        if call not in receving_calls and call not in sending_texts and call not in receiving_texts and not call.startswith("140"):
+            results.add(call)
+    return sorted(results)
+
+
+def solveP4():
+    print("These numbers could be telemarketers: ")
+    receving_calls, sending_texts, receiving_texts, src_calls = getCallAndText()
+    possible_telemarketers = getPossibleTelemarketers(receving_calls, sending_texts, receiving_texts, src_calls)
+    print(possible_telemarketers)
+
+# possible marketer
+# dont send text -> no src_number on text.csv
+# don't receive text -> no dst_number on text.csv
+# don't receive calls -> not dst_number on calls.csv
+
+# possible telemarketers are all the src_number from call.csv
+solveP4()
